593770f301d8ff79381c36caed28d8e2d061b2f3
lairinusReborn <lairinusReborn@users.noreply.github.com>
2018-03-28 21:54:02 -0700

    Initial commit

---
+++ unity3d-transitioner/Assets/Lairinus.Transitioner/Editor/UITransitionerEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(UITransitioner))]
+public class UITransitionerEditor : Editor
+{
+    private AnimationCurve curve = new AnimationCurve();
+
+    private void OnEnable()
+    {
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        curve = EditorGUI.CurveField(new Rect(10, 10, 100, 100), curve);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
